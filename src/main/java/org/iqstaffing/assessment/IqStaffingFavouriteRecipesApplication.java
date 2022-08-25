eef3ff09c1520229ace382fa24a2951dba40c4c3
Petar Gudeski <gudeskipetar@yahoo.com>
2022-08-25 19:35:53 +0200

    intial load

---
+++ src/main/java/org/iqstaffing/assessment/IqStaffingFavouriteRecipesApplication.java
@@ -0,0 +1,13 @@
+package org.iqstaffing.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqStaffingFavouriteRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqStaffingFavouriteRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iqstaffing/assessment/IqStaffingFavouriteRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iqstaffing.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqStaffingFavouriteRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
